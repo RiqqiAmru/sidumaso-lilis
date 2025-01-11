fce5fa5895d0699d946850e459d94635ea9b57a0
RiqqiAmru <Riqqia00@gmail.com>
2025-01-11 22:59:41 +0700

    fix eror datatables empty + modal img not display

--- app/Views/templates/tablePengaduan.php
+++ app/Views/templates/tablePengaduan.php
@@ -17,7 +17,7 @@
       <th scope="col">Gang</th> <!-- Kolom Gang -->
       <th scope="col">Detail Lokasi</th> <!-- Kolom Detail Lokasi -->
       <?php if (session('user_id')['role'] != 'Masyarakat'): ?>
-        <th scope="col">Pengirim</th>
+      <th scope="col">Pengirim</th>
       <?php endif; ?>
       <th scope="col">Status</th>
       <th scope="col">Bukti</th>
@@ -26,40 +26,40 @@
   </thead>
   <tbody>
     <?php if (!empty($pengaduan) && is_array($pengaduan)): ?>
-      <?php $no = 1;
+    <?php $no = 1;
       foreach ($pengaduan as $p): ?>
-        <tr data-id="<?= $p['id'] ?>">
-          <th scope="row"><?= $no++ ?></th>
-          <td><?= $p['created_at'] ?></td>
-          <td><?= $p['jenis_pengaduan'] ?></td>
-          <td><?= $p['rincian'] ?></td>
-          <td><?= $p['gang'] ?></td> <!-- Menampilkan data Gang -->
-          <td><?= $p['detail_lokasi'] ?></td> <!-- Menampilkan data Detail Lokasi -->
-          <?php if (session('user_id')['role'] != 'Masyarakat'): ?>
-            <td><?= $p['nama'] ?></td>
-          <?php endif; ?>
-          <td>
-            <button class="btn">
-              <?php if ($p['ket'] == 0): ?>
-                <span class="badge rounded-pill text-bg-primary btn-tanggapan" data-id="<?= $p['id']; ?>">Menunggu</span>
-              <?php elseif ($p['ket'] == 1): ?>
-                <span class="badge rounded-pill text-bg-secondary btn-tanggapan" data-id="<?= $p['id']; ?>">Proses</span>
-              <?php elseif ($p['ket'] == 2): ?>
-                <span class="badge rounded-pill text-bg-warning btn-tanggapan" data-id="<?= $p['id']; ?>">Menunggu kelengkapan
-                  data</span>
-              <?php elseif ($p['ket'] == 3): ?>
-                <span class="badge rounded-pill text-bg-success btn-tanggapan" data-id="<?= $p['id']; ?>">Selesai</span>
-              <?php elseif ($p['ket'] == 4): ?>
-                <span class="badge rounded-pill text-bg-danger btn-tanggapan" data-id="<?= $p['id']; ?>">Invalid</span>
-              <?php elseif ($p['ket'] == 5 || $p['ket'] == 6): ?>
-                <span class="badge rounded-pill text-bg-secondary btn-tanggapan" data-id="<?= $p['id']; ?>">Menunggu
-                  admin</span>
-              <?php endif ?>
-            </button>
-          </td>
-          <td>
-            <?php foreach ($p['foto'] as $foto): ?>
-              <?php
+    <tr data-id="<?= $p['id'] ?>">
+      <th scope="row"><?= $no++ ?></th>
+      <td><?= $p['created_at'] ?></td>
+      <td><?= $p['jenis_pengaduan'] ?></td>
+      <td><?= $p['rincian'] ?></td>
+      <td><?= $p['gang'] ?></td> <!-- Menampilkan data Gang -->
+      <td><?= $p['detail_lokasi'] ?></td> <!-- Menampilkan data Detail Lokasi -->
+      <?php if (session('user_id')['role'] != 'Masyarakat'): ?>
+      <td><?= $p['nama'] ?></td>
+      <?php endif; ?>
+      <td>
+        <button class="btn">
+          <?php if ($p['ket'] == 0): ?>
+          <span class="badge rounded-pill text-bg-primary btn-tanggapan" data-id="<?= $p['id']; ?>">Menunggu</span>
+          <?php elseif ($p['ket'] == 1): ?>
+          <span class="badge rounded-pill text-bg-secondary btn-tanggapan" data-id="<?= $p['id']; ?>">Proses</span>
+          <?php elseif ($p['ket'] == 2): ?>
+          <span class="badge rounded-pill text-bg-warning btn-tanggapan" data-id="<?= $p['id']; ?>">Menunggu kelengkapan
+            data</span>
+          <?php elseif ($p['ket'] == 3): ?>
+          <span class="badge rounded-pill text-bg-success btn-tanggapan" data-id="<?= $p['id']; ?>">Selesai</span>
+          <?php elseif ($p['ket'] == 4): ?>
+          <span class="badge rounded-pill text-bg-danger btn-tanggapan" data-id="<?= $p['id']; ?>">Invalid</span>
+          <?php elseif ($p['ket'] == 5 || $p['ket'] == 6): ?>
+          <span class="badge rounded-pill text-bg-secondary btn-tanggapan" data-id="<?= $p['id']; ?>">Menunggu
+            admin</span>
+          <?php endif ?>
+        </button>
+      </td>
+      <td>
+        <?php foreach ($p['foto'] as $foto): ?>
+        <?php
               // Dapatkan ekstensi file
               $file_extension = pathinfo($foto, PATHINFO_EXTENSION);
               // Daftar ekstensi gambar yang diperbolehkan
@@ -67,119 +67,123 @@
               // Periksa apakah file tersebut termasuk dalam ekstensi gambar
               if (in_array(strtolower($file_extension), $allowed_image_extensions)):
               ?>
-                <!-- Jika file adalah gambar, tampilkan gambar -->
-                <img src="<?= base_url('uploads/bukti/' . $foto) ?>" alt="Foto bukti" class="img-thumbnail" width="100"
-                  data-bs-toggle="modal" data-bs-target="#imageModal">
-              <?php else: ?>
-                <!-- Jika bukan gambar, tampilkan sesuatu lain (misalnya teks atau icon) -->
-                <a href="<?= base_url('uploads/bukti/' . $foto) ?>" target="_blank" rel="noopener noreferrer">download
-                  <?= $file_extension ?></a>
-              <?php endif; ?>
-            <?php endforeach; ?>
-          </td>
+        <!-- Jika file adalah gambar, tampilkan gambar -->
+        <img src="<?= base_url('uploads/bukti/' . $foto) ?>" alt="Foto bukti" class="img-thumbnail" width="100"
+          data-bs-toggle="modal" data-bs-target="#imageModal">
+        <?php else: ?>
+        <!-- Jika bukan gambar, tampilkan sesuatu lain (misalnya teks atau icon) -->
+        <a href="<?= base_url('uploads/bukti/' . $foto) ?>" target="_blank" rel="noopener noreferrer">download
+          <?= $file_extension ?></a>
+        <?php endif; ?>
+        <?php endforeach; ?>
+      </td>
 
-          <td>
-            <?php if (session('user_id')['role'] == 'Masyarakat' && $p['ket'] == 0): ?>
-              <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalDeletePengaduan"
-                data-bs-id='<?= $p['id'] ?>'>
-                Hapus</button>
-              <button class="btn btn-outline-info ">
-                <a href="<?= base_url('pengaduan/edit/' . $p['id']) ?>">Edit</a></button>
-            <?php elseif ((session('user_id')['role'] == 'Admin' | session('user_id')['role'] == 'Kepala_dusun') && $p['ket'] == 0): ?>
-              <button class="btn btn-outline-success">
-                <a href="<?= base_url('pengaduan/proses/' . $p['id']) ?>">Proses</a></button>
-            <?php endif ?>
-          </td>
+      <td>
+        <?php if (session('user_id')['role'] == 'Masyarakat' && $p['ket'] == 0): ?>
+        <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalDeletePengaduan"
+          data-bs-id='<?= $p['id'] ?>'>
+          Hapus</button>
+        <button class="btn btn-outline-info ">
+          <a href="<?= base_url('pengaduan/edit/' . $p['id']) ?>">Edit</a></button>
+        <?php elseif ((session('user_id')['role'] == 'Admin' | session('user_id')['role'] == 'Kepala_dusun') && $p['ket'] == 0): ?>
+        <button class="btn btn-outline-success">
+          <a href="<?= base_url('pengaduan/proses/' . $p['id']) ?>">Proses</a></button>
+        <?php endif ?>
+      </td>
 
-        </tr>
-      <?php endforeach; ?>
-    <?php else: ?>
-      <tr>
-        <td colspan="9" class="text-center">Belum ada data pengaduan.</td>
-      </tr>
+    </tr>
+    <?php endforeach; ?>
     <?php endif; ?>
   </tbody>
 </table>
 <style>
-  table#pengaduanTable {
-    border-collapse: collapse;
-    width: 100%;
-  }
+table#pengaduanTable {
+  border-collapse: collapse;
+  width: 100%;
+}
 
-  table#pengaduanTable th,
-  table#pengaduanTable td {
-    padding: 10px;
-    border: 1px solid #ddd;
-    text-align: center;
-  }
+table#pengaduanTable th,
+table#pengaduanTable td {
+  padding: 10px;
+  border: 1px solid #ddd;
+  text-align: center;
+}
 
-  table#pengaduanTable th {
-    background-color: #f4f4f4;
-    font-weight: bold;
-  }
+table#pengaduanTable th {
+  background-color: #f4f4f4;
+  font-weight: bold;
+}
 
-  table#pengaduanTable {
-    table-layout: auto;
-    /* Mengatur lebar kolom otomatis */
-    word-wrap: break-word;
-    /* Jika teks terlalu panjang, otomatis dipotong */
-  }
+table#pengaduanTable {
+  table-layout: auto;
+  /* Mengatur lebar kolom otomatis */
+  word-wrap: break-word;
+  /* Jika teks terlalu panjang, otomatis dipotong */
+}
 </style>
 <script>
-  $(document).ready(function() {
-    // Inisialisasi DataTables
-    $('#pengaduanTable').DataTable({
-      "paging": true,
-      "searching": true,
-      "ordering": true,
-      "info": true,
-      "lengthMenu": [5, 10, 25, 50],
-      "language": {
-        "search": "Cari:",
-        "lengthMenu": "Tampilkan _MENU_ data per halaman",
-        "zeroRecords": "Data tidak ditemukan",
-        "info": "Menampilkan _START_ hingga _END_ dari _TOTAL_ data",
-        "infoEmpty": "Tidak ada data tersedia",
-        "infoFiltered": "(difilter dari _MAX_ total data)",
-        "paginate": {
-          "first": "Awal",
-          "last": "Akhir",
-          "next": "Berikutnya",
-          "previous": "Sebelumnya"
-        }
+$(document).ready(function() {
+  // Inisialisasi DataTables
+  $('#pengaduanTable').DataTable({
+    "paging": true,
+    "searching": true,
+    "ordering": true,
+    "info": true,
+    "lengthMenu": [5, 10, 25, 50],
+    "language": {
+      "search": "Cari:",
+      "lengthMenu": "Tampilkan _MENU_ data per halaman",
+      "zeroRecords": "Data tidak ditemukan",
+      "info": "Menampilkan _START_ hingga _END_ dari _TOTAL_ data",
+      "infoEmpty": "Tidak ada data tersedia",
+      "infoFiltered": "(difilter dari _MAX_ total data)",
+      "paginate": {
+        "first": "Awal",
+        "last": "Akhir",
+        "next": "Berikutnya",
+        "previous": "Sebelumnya"
+      }
+    },
+    "columns": [{
+        "data": "no"
       },
-      "columns": [{
-          "data": "no"
-        },
-        {
-          "data": "tanggal"
-        },
-        {
-          "data": "perihal"
-        },
-        {
-          "data": "rincian"
-        },
-        {
-          "data": "gang"
-        },
-        {
-          "data": "detail_lokasi"
-        },
-        <?php if (session('user_id')['role'] != 'Masyarakat'): ?> {
-            "data": "pengirim"
-          }, // Pengirim hanya muncul jika bukan masyarakat
-        <?php endif ?> {
-          "data": "status"
-        },
-        {
-          "data": "bukti"
-        }, {
-          "data": "aksi"
-        }
-      ]
+      {
+        "data": "tanggal"
+      },
+      {
+        "data": "perihal"
+      },
+      {
+        "data": "rincian"
+      },
+      {
+        "data": "gang"
+      },
+      {
+        "data": "detail_lokasi"
+      },
+      <?php if (session('user_id')['role'] != 'Masyarakat'): ?> {
+        "data": "pengirim"
+      }, // Pengirim hanya muncul jika bukan masyarakat
+      <?php endif ?> {
+        "data": "status"
+      },
+      {
+        "data": "bukti"
+      }, {
+        "data": "aksi"
+      }
+    ]
+  });
+  document.querySelectorAll('img[data-bs-toggle="modal"]').forEach(img => {
+    img.addEventListener('click', function() {
+      console.log(this.src);
+      console.log('clicked2');
+      const modalImage = document.getElementById('modalImage');
+      modalImage.src = this.src;
     });
   });
+});
 </script>
 
 <!-- modal image -->
@@ -195,34 +199,34 @@
 
 
 <script>
-  document.addEventListener('DOMContentLoaded', function() {
-    const table = document.getElementById('pengaduanTable');
-    table.addEventListener('click', function(e) {
+document.addEventListener('DOMContentLoaded', function() {
+  const table = document.getElementById('pengaduanTable');
+  table.addEventListener('click', function(e) {
 
-      if (e.target && e.target.classList.contains('btn-tanggapan')) {
-        const idAduan = e.target.dataset.id;
-        console.log(idAduan);
-        const row = e.target.closest('tr');
+    if (e.target && e.target.classList.contains('btn-tanggapan')) {
+      const idAduan = e.target.dataset.id;
+      console.log(idAduan);
+      const row = e.target.closest('tr');
 
-        // Periksa apakah sudah ada tanggapan di bawahnya
-        if (row.nextElementSibling && row.nextElementSibling.classList.contains('tanggapan-row')) {
-          row.nextElementSibling.remove();
-          return;
-        }
+      // Periksa apakah sudah ada tanggapan di bawahnya
+      if (row.nextElementSibling && row.nextElementSibling.classList.contains('tanggapan-row')) {
+        row.nextElementSibling.remove();
+        return;
+      }
 
-        // Hapus semua tanggapan lain yang terbuka
-        document.querySelectorAll('.tanggapan-row').forEach(el => el.remove());
+      // Hapus semua tanggapan lain yang terbuka
+      document.querySelectorAll('.tanggapan-row').forEach(el => el.remove());
 
-        // Fetch tanggapan dari server
-        fetch(`/tanggapan/${idAduan}`)
-          .then(response => response.json())
-          .then(data => {
+      // Fetch tanggapan dari server
+      fetch(`/tanggapan/${idAduan}`)
+        .then(response => response.json())
+        .then(data => {
 
-            if (data) {
-              const tanggapanRow = document.createElement('tr');
-              tanggapanRow.classList.add('tanggapan-row');
+          if (data) {
+            const tanggapanRow = document.createElement('tr');
+            tanggapanRow.classList.add('tanggapan-row');
 
-              tanggapanRow.innerHTML = `
+            tanggapanRow.innerHTML = `
                 <td></td>
                                 <td colspan="6">
                                     <table class="table table-bordered">
@@ -266,7 +270,7 @@
                                                      <a href="<?= base_url('pengaduan/proses/') ?>${tanggapan.id_aduan}/lengkapi" class="btn badge rounded-pill text-bg-info " >lengkapi data</a>
                                                      `: ''}
                                         ${index === data.length - 1 && (tanggapan.jenis_tanggapan == 'Proses') && '<?= session('user_id')['role'] ?>' == 'Masyarakat' ? `
-<a href="<?= base_url('pengaduan/proses/') ?>${tanggapan.id_aduan}/komentar" class="btn badge rounded-pill text-bg-primary " >komentar</a> </br>
+      <a href="<?= base_url('pengaduan/proses/') ?>${tanggapan.id_aduan}/komentar" class="btn badge rounded-pill text-bg-primary " >komentar</a> </br>
                                                      `: ''}
 
                                                     </td>
@@ -276,28 +280,30 @@
                                     </table>
                                 </td>
                             `;
-              row.after(tanggapanRow);
-              document.querySelectorAll('img[data-bs-toggle="modal"]').forEach(img => {
-                img.addEventListener('click', function() {
-                  console.log(this.src);
-                  const modalImage = document.getElementById('modalImage');
-                  modalImage.src = this.src;
-                });
+            row.after(tanggapanRow);
+            document.querySelectorAll('img[data-bs-toggle="modal"]').forEach(img => {
+              img.addEventListener('click', function() {
+                console.log(this.src);
+                console.log('clicked');
+                const modalImage = document.getElementById('modalImage');
+                modalImage.src = this.src;
               });
-            } else {
-              alert('Tidak ada tanggapan untuk pengaduan ini.');
-            }
-          })
-          .catch(err => console.error('Error fetching tanggapan:', err));
-      }
-    });
+            });
+          } else {
+            alert('Tidak ada tanggapan untuk pengaduan ini.');
+          }
+        })
+        .catch(err => console.error('Error fetching tanggapan:', err));
+    }
+  });
 
-    document.querySelectorAll('img[data-bs-toggle="modal"]').forEach(img => {
-      img.addEventListener('click', function() {
-        console.log(this.src);
-        const modalImage = document.getElementById('modalImage');
-        modalImage.src = this.src;
-      });
+  document.querySelectorAll('img[data-bs-toggle="modal"]').forEach(img => {
+    img.addEventListener('click', function() {
+      console.log(this.src);
+      console.log('clicked2');
+      const modalImage = document.getElementById('modalImage');
+      modalImage.src = this.src;
     });
   });
+});
 </script>
